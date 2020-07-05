6e0022bd031eb36c917f2929d134a5e6fa7bc279
soroush <soroushtabatabaee@icloud.com>
2020-07-05 11:24:43 +0430

    first commit

--- src/com/company/Circuit.java
+++ src/com/company/Circuit.java
@@ -6,4 +6,8 @@ import java.util.HashSet;
 public class Circuit {
     HashSet<Node> nodes= new HashSet<Node>();
 
+    public Circuit(HashSet<Node> nodes) {
+        this.nodes = nodes;
+        System.out.println("sdojfdpkulhlikjzsdpdsgoi");
+    }
 }
